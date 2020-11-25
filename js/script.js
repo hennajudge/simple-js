2ce7bfcb9c7baddbde009352f542959cec6dcbfb
hennajudge <hennajudge@github.com>
2020-11-24 18:25:43 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+let pokemonList = [
+    // pokemon objects
+  ];
+  
+  for (let i = 0; i < pokemonList.length; i++){
+    // document.write("<p>" + pokemonList[i].name + "</p>");
+    // printing pokemonList[i]’s other details
+    // ...
+  }
+  
+  for (let i = 0; i < pokemonList.length; i++){
+    // document.write("<p>" + pokemonList[i].name + "</p>");
+    // printing pokemonList[i]’s other details
+    // ...
+  }
+    {
+        name: 'Bulbasaur',
+        height;0.7,
+        types ['grass','poision']
+    }
+    
+    {
+        name: 'Charizard',
+        height;1.7,
+        types  ['fire', 'fyling']
+    }
+    
+    {
+        name:'squirtle',
+        height;1,
+        types ['water']
+    }
+    
+    console.log(pokemonList
+        )
+    //new
