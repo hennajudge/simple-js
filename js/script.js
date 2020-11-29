2813319da443c2d27e6a70b80c8e55407aa5f477
hennajudge <hennajudge@github.com>
2020-11-29 02:07:19 -0800

    new code

--- js/script.js
+++ js/script.js
@@ -9,12 +9,33 @@ let pokemonList2 = [
   { name: "bird", height: 0.7, types: ["grass"] },
   { name: "cat", height: 0.6, types: ["fire"] },
   { name: "dog", height: 1.6, types: ["flying", "poison"] },
-];
 
 function printArrayDetails(list){
   for (let i = 0; i < list.length; i++){
     document.write("<p>" + list[i].name + "</p>")
     console.log(list[i].name);
+    function add(num1, num2) {
+      return num1+num2;
+    }
+    
+    function subtract(num1, num2) {
+      return num1-num2;
+    }
+    
+    function multiply(num1, num2) {
+      return num1*num2;
+    }
+    
+    function divide(num1, num2){
+      if(num2===0){
+        return "Not Allowed"
+      }
+      else {
+        return num1/num2;
+      }
+    }
+    
+    
   }
 }
 
