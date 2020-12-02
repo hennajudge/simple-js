bc82a5f56aaef9973ca174b522c524781254676d
hennajudge <hennajudge@github.com>
2020-12-02 00:09:39 -0800

    Update script.js
    added 1.3 p3 task, removed calculator function.

--- js/script.js
+++ js/script.js
@@ -1,34 +1,13 @@
 let pokemonList = [
   { name: "Bulbasaur", height: 0.7, types: ["grass"] },
   { name: "Charmander", height: 0.6, types: ["fire"] },
-  { name: "Golbat", height: 1.6, types: ["flying", "poison"] },
+  { name: "Golbat", height: 1.6,  types: ["flying", "poison"] },
   { name: "Alakazam", height: 1.5, types: ["psychic"] },
 ];
-function printArrayDetails(list){
-  for (let i = 0; i < list.length; i++){
-    document.write("<p>" + list[i].name + "</p>")
-    console.log(list[i].name);
-    function add(num1, num2) {
-      return num1+num2;
-    }
-    
-    function subtract(num1, num2) {
-      return num1-num2;
-    }
-    
-    function multiply(num1, num2) {
-      return num1*num2;
-    }
-    
-    function divide(num1, num2){
-      if(num2===0){
-        return "Not Allowed"
-      }
-      else {
-        return num1/num2;
-      }
-    }
-    
-    
-  }
+if (height ==1.6) {
+  console.log('wow thats big!');
 }
+  for (let i = 0; i < list.length; i++){
+    document.write("<p>" + pokemonList[i].name + "</p>")
+  }
+  
