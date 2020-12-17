e0aa5f3e8349b29e64c0646e14cd8dac2a77261c
hennajudge <hennajudge@github.com>
2020-12-17 00:48:54 -0800

    added buttons 1.6

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 let pokemonRepository = (function (){
+
+
   let pokemonList = [
     {
       name: "Bulbasar",
@@ -18,20 +20,46 @@ let pokemonRepository = (function (){
     
   ];
 
+let pokemonRepository = (function () {
+  let list = [];
+
   function add(pokemon) {
-    pokemonList.push(pokemon);
+    list.push(pokemon);
   }
 
+  function addListItem(pokemon) {
+    let list = document.querySelector(".pokemon-list");
+    let listItem = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button");
+    listItem.appendChild(button);
+    list.appendChild(listItem);
+    button.addEventListener("click", function (event) {
+      showDetails(pokemon);
+    });
+  }
+  function showDetails(pokemon) {
+    console.log(pokemon.name);
+  }
   function getAll() {
-    return pokemonList;
+    return list;
   }
 
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem,
   };
 })();
 
-pokemonRepository.getAll().forEach(function(pokemon) {
-  document.write(pokemon.name + " (type: " + pokemon.type + ")" + " (height " + pokemon.height + ")" + "<br>");
+pokemonList.forEach(function (pokemon) {
+  pokemonRepository.add(pokemon);
+
 });
+let allPokemon = pokemonRepository.getAll();
+console.log("my initial pokemon list: ", allPokemon);
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
+})})
